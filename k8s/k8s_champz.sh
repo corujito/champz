2583f1c41cb56c2b844e79add7345b0c3b7a6c4c
Kleber Manoel Infante <corujito@gmail.com>
2018-09-03 13:57:14 -0400

    k8s files

--- k8s/k8s_champz.sh
+++ k8s/k8s_champz.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+############ delete
+
 kubectl delete -f champz-web-service.yaml
 kubectl delete -f champz-web-dep.yaml
 kubectl delete -f champz-web-postgresdb-service.yaml
@@ -13,15 +15,17 @@ kubectl delete -f champz-restservice-mongodb-dep.yaml
 kubectl delete -f champz-restservice-secret.yaml
 kubectl delete -f champz-restservice-mongodb-secret.yaml
 
-#kubectl create -f champz-restservice-mongodb-secret.yaml
-#kubectl create -f champz-restservice-secret.yaml
+############ create
 
-#kubectl create -f champz-restservice-mongodb-dep.yaml
-#kubectl create -f champz-restservice-mongodb-service.yaml
-#kubectl create -f champz-restservice-dep.yaml
-#kubectl create -f champz-restservice-service.yaml
+kubectl create -f champz-restservice-mongodb-secret.yaml
+kubectl create -f champz-restservice-secret.yaml
 
-#kubectl create -f champz-web-postgresdb-dep.yaml
-#kubectl create -f champz-web-postgresdb-service.yaml
-#kubectl create -f champz-web-dep.yaml
-#kubectl create -f champz-web-service.yaml
+kubectl create -f champz-restservice-mongodb-dep.yaml
+kubectl create -f champz-restservice-mongodb-service.yaml
+kubectl create -f champz-restservice-dep.yaml
+kubectl create -f champz-restservice-service.yaml
+
+kubectl create -f champz-web-postgresdb-dep.yaml
+kubectl create -f champz-web-postgresdb-service.yaml
+kubectl create -f champz-web-dep.yaml
+kubectl create -f champz-web-service.yaml
